e4186f09c4e7ef00b3a7b5f8433d968be7e2687e
Andrew Laskey <andrew@idiom.co>
2016-12-04 15:12:27 -0800

    Move search into child component

---
+++ src/components/Search.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="album-search">
+    <form v-on:submit.prevent="searchSpotify">
+      <input type="text" v-model="search.album" placeholder="Album Name">
+      <input type="text" v-model="search.artist" placeholder="Artist">
+      <button type="submit">Search</button>
+    </form>
+    <div class="search-results">
+      <ul>
+        <li v-for="album in albumResults">
+          <img v-bind:src="album.image" width="250" height="250">
+          <p><span>{{album.artist}}</span> - <span>{{album.name}}</span></p>
+          <button v-on:click="selectAlbum(album)">Select</button>
+        </li>
+      </ul>
+      <p v-if="noResults">Sorry, there are no matching albums.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'search',
+  data () {
+    return {
+      search: {
+        album: '',
+        artist: ''
+      },
+      albumResults: [],
+      noResults: false,
+    }
+  },
+  methods: {
+    searchSpotify: function () {
+      var _this = this;
+      var query = 'album:' + this.search.album + ' artist:' + this.search.artist;
+
+      axios.get('https://api.spotify.com/v1/search',{
+        params: {
+          type: 'album',
+          q: query
+        }
+      })
+      .then(function (response) {
+        if (response.status === 200) {
+          if (response.data.albums.items.length === 0) {
+            _this.noResults = true;
+          } else {
+            _this.noResults = false;
+            _this.albumResults = response.data.albums.items.map( album => {
+              return {
+                artist: album.artists.name,
+                name: album.name,
+                image: album.images[0].url,
+                id: album.id,
+                url: album.external_urls.spotify
+              }
+            });
+          }
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    selectAlbum: function (album) {
+      this.$emit('selectAlbum', album);
+    }
+  }
+}
+</script>
--- src/components/VoteView.vue
+++ src/components/VoteView.vue
@@ -1,70 +1,30 @@
 <template>
 	<div class="vote">
-		<form v-on:submit.prevent="searchSpotify">
-			<input type="text" v-model="search.album" placeholder="Album Name">
-			<input type="text" v-model="search.artist" placeholder="Artist">
-			<button type="submit">Search</button>
-		</form>
-		<div class="search-results">{{searchResults}}
-			<ul>
-				<li v-for="album in albumResults">
-					<img v-bind:src="album.image" width="250" height="250">
-					<p><span>{{album.artist}}</span> - <span>{{album.name}}</span></p>
-				</li>
-			</ul>
-			<p v-if="noResults">Sorry, there are no matching albums.</p>
-		</div>
+		<search v-on:selectAlbum="addAlbum"></search>
+		<ol>
+			<li v-for="album in albums">
+				{{album}}
+			</li>
+		</ol>
 	</div>
 </template>
 
 <script>
-import axios from 'axios'
+import Search from './Search.vue'
 
 export default {
 	name: 'vote-view',
 	data () {
 		return {
-			search: {
-				album: '',
-				artist: ''
-			},
-			albumResults: [],
-			noResults: false,
 			albums: []
 		}
 	},
+	components: {
+		Search
+	},
 	methods: {
-		searchSpotify: function () {
-			var _this = this;
-			var query = 'album:' + this.search.album + ' artist:' + this.search.artist;
-
-			axios.get('https://api.spotify.com/v1/search',{
-				params: {
-					type: 'album',
-					q: query
-				}
-			})
-			.then(function (response) {
-				if (response.status === 200) {
-					if (response.data.albums.items.length === 0) {
-						_this.noResults = true;
-					} else {
-						_this.noResults = false;
-						_this.albumResults = response.data.albums.items.map( album => {
-							return {
-								artist: album.artists.name,
-								name: album.name,
-								image: album.images[0].url,
-								id: album.id,
-								url: album.external_urls.spotify
-							}
-						});
-					}
-				}
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
+		addAlbum: function(album) {
+			this.albums.push(album)
 		}
 	}
 }
