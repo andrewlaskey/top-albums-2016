73ab8d4c14bef9b7615cced32e33c4389ae160d2
Andrew Laskey <andrew@idiom.co>
2016-12-04 15:58:14 -0800

    Add Bootstrap

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="album-search">
-    <form v-on:submit.prevent="searchSpotify">
-      <input type="text" v-model="search.album" placeholder="Album Name">
-      <input type="text" v-model="search.artist" placeholder="Artist">
-      <button type="submit">Search</button>
+    <form v-on:submit.prevent="searchSpotify" class="form-inline">
+      <div class="form-group">
+        <label class="sr-only" for="albumName">Album Name</label>
+        <input class="form-control" type="text" v-model="search.album" name="albumName" placeholder="Album Name">
+      </div>
+      <div class="form-group">
+        <label class="sr-only" for="artistName">Artist Name</label>
+        <input class="form-control" type="text" v-model="search.artist" name="artistName" placeholder="Artist">
+      </div>
+      <button type="submit" class="btn btn-primary">Search</button>
     </form>
     <div class="search-results">
-      <ul>
-        <li v-for="album in albumResults">
-          <img v-bind:src="album.image" width="250" height="250">
-          <p><span>{{album.artist}}</span> - <span>{{album.name}}</span></p>
-          <button v-on:click="selectAlbum(album)">Select</button>
+      <ul class="list-unstyled">
+        <li v-for="album in albumResults" class="row">
+          <div class="col-sm-4">
+            <img v-bind:src="album.image" class="img-responsive img-rounded">
+          </div>
+          <div class="col-sm-8">
+            <h3><span>{{album.artist}}</span> - <span>{{album.name}}</span></h3>
+            <button v-on:click="selectAlbum(album)" class="btn btn-default">Select</button>
+          </div>
         </li>
       </ul>
       <p v-if="noResults">Sorry, there are no matching albums.</p>
@@ -50,10 +60,11 @@ export default {
           if (response.data.albums.items.length === 0) {
             _this.noResults = true;
           } else {
+            console.log(response);
             _this.noResults = false;
             _this.albumResults = response.data.albums.items.map( album => {
               return {
-                artist: album.artists.name,
+                artist: album.artists[0].name,
                 name: album.name,
                 image: album.images[0].url,
                 id: album.id,
--- src/components/VoteView.vue
+++ src/components/VoteView.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="vote">
-    <search v-on:selectAlbum="addAlbum" :enabled="albums.length < maxAlbums"></search>
-    <ol>
-      <li v-for="album in albums">
-        {{album}}
-      </li>
-    </ol>
+  <div class="vote row">
+    <div class="col-md-6">
+      <h2>Search</h2>
+      <p>Search for and add albums on Spotify</p>
+      <search v-on:selectAlbum="addAlbum" :enabled="albums.length < maxAlbums"></search>
+    </div>
+    <div class="col-md-6">
+      <h2>Vote</h2>
+      <p>Sort and submit your favorite albums</p>
+      <ul class="list-group">
+        <li v-for="(album, index) in albums" class="list-group-item">
+          <div class="row">
+            <div class="col-sm-3">
+              <img v-bind:src="album.image" class="img-responsive img-rounded">
+            </div>
+            <div class="col-sm-9">
+              <h3>{{index + 1}}. <span>{{album.artist}}</span> - <span>{{album.name}}</span></h3>
+              <button class="btn btn-danger btn-sm">Remove</button>
+            </div>
+          </div>
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
