56749449c85d08c7f69105a4990097d4696a70cd
Andrew Laskey <andrew@idiom.co>
2016-12-12 16:55:00 -0800

    Add album vote sorting and removal

--- src/components/VoteView.vue
+++ src/components/VoteView.vue
@@ -9,14 +9,20 @@
       <h2>Vote</h2>
       <p>Sort and submit your favorite albums</p>
       <ul class="list-group">
-        <li v-for="(album, index) in albums" class="list-group-item">
+        <li v-for="(album, index) in albums" :key="albums.id" class="list-group-item">
           <div class="row">
             <div class="col-sm-3">
               <img v-bind:src="album.image" class="img-responsive img-rounded">
             </div>
             <div class="col-sm-9">
               <h3>{{index + 1}}. <span>{{album.artist}}</span> - <span>{{album.name}}</span></h3>
-              <button class="btn btn-danger btn-sm">Remove</button>
+              <button class="btn btn-danger btn-sm" v-on:click="removeAlbum(index)">Remove</button>
+              <button type="button" class="btn btn-default" aria-label="Move Up" v-if="index !== 0" v-on:click="moveUp(index)">
+                <span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>
+              </button>
+              <button type="button" class="btn btn-default" aria-label="Move Down" v-if="index !== albums.length - 1" v-on:click="moveDown(index)">
+                <span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span>
+              </button>
             </div>
           </div>
         </li>
@@ -51,6 +57,23 @@ export default {
 
       if (!duplicate) {
         this.albums.push(album)
+      }
+    },
+    removeAlbum: function (index) {
+      this.albums.splice(index, 1)
+    },
+    moveUp: function (index) {
+      if (index !== 0) {
+        let temp = this.albums[index]
+        this.albums.splice(index, 1, this.albums[index - 1])
+        this.albums.splice(index - 1, 1, temp)
+      }
+    },
+    moveDown: function (index) {
+      if (index !== this.albums.length - 1) {
+        let temp = this.albums[index]
+        this.albums.splice(index, 1, this.albums[index + 1])
+        this.albums.splice(index + 1, 1, temp)
       }
     }
   }
